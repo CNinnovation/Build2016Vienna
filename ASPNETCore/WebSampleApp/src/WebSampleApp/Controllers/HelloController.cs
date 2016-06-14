14e72ff82868e098b1ea3dc8815af69bd0144f46
Christian Nagel <christian@christiannagel.com>
2016-06-14 21:47:53 +0200

    ASP.NET Core sample app

---
+++ ASPNETCore/WebSampleApp/src/WebSampleApp/Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebSampleApp.Services;
+
+namespace WebSampleApp.Controllers
+{
+    public class HelloController
+    {
+        private IHelloService _helloService;
+        public HelloController(IHelloService helloService)
+        {
+            _helloService = helloService;
+        }
+        public string Index(string name)
+        {
+            return $"<h2>{_helloService.Hello(name)}</h2>";
+        }
+    }
+}
---
+++ ASPNETCore/WebSampleApp/src/WebSampleApp/Middleware/CustomHeadingMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace WebSampleApp.Middleware
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class CustomHeadingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomHeadingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            httpContext.Response.Headers.Add("myheader", "my app");
+            return _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class CustomHeadingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomHeadingMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomHeadingMiddleware>();
+        }
+    }
+}
